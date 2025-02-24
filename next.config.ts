534efab0f28bd5e539defa72be8236f77d68342d
bible <bibleza555@gmail.com>
2025-02-25 02:10:19 +0700

    fix import font and img

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: "/countdown-babi", 
-  basePath: "/countdown-babi", // แก้เป็นชื่อ repository ของคุณ
 };
 
 export default nextConfig;
