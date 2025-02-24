160f318e977c64a4813e3a0e5c4edf7b0faaf338
bible <bibleza555@gmail.com>
2025-02-25 01:23:25 +0700

    add export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/your-repo-name", // แก้เป็นชื่อ repository ของคุณ
+  images: {
+    unoptimized: true, // จำเป็นสำหรับ Next.js Export
+  },
 };
 
 export default nextConfig;
